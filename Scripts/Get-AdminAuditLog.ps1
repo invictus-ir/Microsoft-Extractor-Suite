b87f1ff77605f4852a6bde47408f2b20b582f26f
angry-bender <angry.licence@gmail.com>
2024-07-11 23:24:06 +0930

    Update Get-AdminAuditLog.ps1
    change break to throw as per #80

--- Scripts/Get-AdminAuditLog.ps1
+++ Scripts/Get-AdminAuditLog.ps1
@@ -70,7 +70,7 @@ function Get-AdminAuditLog {
 	catch {
 		write-logFile -Message "[INFO] Ensure you are connected to M365 by running the Connect-M365 command before executing this script" -Color "Yellow"
 		Write-logFile -Message "[ERROR] An error occurred: $($_.Exception.Message)" -Color "Red"
-		break
+		throw
 	}   
 
     write-logFile -Message "[INFO] Output is written to: $outputDirectory" -Color "Green"
