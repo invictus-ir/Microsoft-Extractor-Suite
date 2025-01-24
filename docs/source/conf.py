6b15da6d4e15d383add1da0206579db77b51e3ec
JoeyInvictus <129975292+JoeyInvictus@users.noreply.github.com>
2025-01-24 16:04:22 +0100

    Forgot to update the version number.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'Microsoft-Extractor-Suite'
 copyright = 'Copyright 2025 Invictus Incident Response'
 author = 'Joey Rentenaar & Korstiaan Stam'
 
-release = '3.0.0'
-version = '3.0.0'
+release = '3.0.1'
+version = '3.0.1'
 
 # -- General configuration
 
