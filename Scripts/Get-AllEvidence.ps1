c6fdaecb1982c93120d67c00caa98a411f5f2586
k8pl3r-sh <k8pl3r@gmail.com>
2025-10-29 14:46:05 +0100

    fix: add hardcoded list of commands supporting -Output + add if/else to execute with right parameters

--- Scripts/Get-AllEvidence.ps1
+++ Scripts/Get-AllEvidence.ps1
@@ -725,13 +725,20 @@ function Start-EvidenceCollection {
     Write-LogFile -Message "Start Time: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" -Level Minimal
     Write-LogFile -Message "----------------------------------------" -Level Minimal
 
+    # List of tasks supporting Output parameter
+    $tasksWithOutput = @('Devices','SignInLogs','AuditLogs','UnifiedAuditLog')
+
     if ($Platform -eq "All" -or $Platform -eq "Azure") {
         Write-LogFile -Message "`n==== Starting Azure/Entra ID Data Collection ====" -Color "Yellow" -Level Minimal
         foreach ($task in $Global:CollectionTasks.Azure.GetEnumerator()) {
             if ($task.Value.Enabled) {
                 try {
                     $script:CollectionOutputDir = $OutputDir
-                    $executed = & $task.Value.Function -LogLevel $LogLevel -UserIds $UserIds -Output $Output
+                    if ($tasksWithOutput -contains $task.Key) {
+                        $executed = & $task.Value.Function -OutputDir $OutputDir -LogLevel $LogLevel -UserIds $UserIds -Output $Output
+                    } else {
+                        $executed = & $task.Value.Function -OutputDir $OutputDir -LogLevel $LogLevel -UserIds $UserIds
+                    }
                     if ($executed) {
                         $summary.TotalTasks++
                         $summary.SuccessfulTasks++
@@ -753,7 +760,11 @@ function Start-EvidenceCollection {
             if ($task.Value.Enabled) {
                 try {
                     $script:CollectionOutputDir = $OutputDir
-                    $executed = & $task.Value.Function -LogLevel $LogLevel -UserIds $UserIds -Output $Output
+                    if ($tasksWithOutput -contains $task.Key) {
+                        $executed = & $task.Value.Function -OutputDir $OutputDir -LogLevel $LogLevel -UserIds $UserIds -Output $Output
+                    } else {
+                        $executed = & $task.Value.Function -OutputDir $OutputDir -LogLevel $LogLevel -UserIds $UserIds
+                    }
                     if ($executed) {
                         $summary.TotalTasks++
                         $summary.SuccessfulTasks++
